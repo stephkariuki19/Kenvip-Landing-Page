10439814a3175af54a8a7092c831257618e30cd2
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-08-17 00:50:46 +0300

    corrected responsiveness

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ setInterval(function(){
   }
 }, 8000);
 
+// Add an event listener to the "Back to the top" link
+document.querySelector('.top').addEventListener('click', () => {
+  // Scroll back to the top of the page
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
+
